397389987aed4856ce9fb0c5f730c154718c8779
Singh2655 <singhshaurya222002@gmail.com>
2024-07-10 23:05:18 +0530

    s3-trigger

---
+++ s3-trigger/index.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports.handler = async (event) => {
+  try {
+    console.log(event);
+    const s3EventData = event.Records[0].s3;
+
+    console.log("Received S3 event with object details:", s3EventData);
+
+    const response = await axios.post(process.env.API_ENDPOINT, {
+      s3EventData,
+    });
+    console.log(
+      "API response received:",
+      `Status: ${response.status}, Data: ${JSON.stringify(response.data)}`
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify("S3 event process successful"),
+    };
+  } catch (error) {
+    console.error("Error processing S3 object data:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Error processing S3 upload"),
+    };
+  }
+};
